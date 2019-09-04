089654d36f2ee76dad833117e59d8d909aa2a8b3
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-09-04 17:53:10 +0500

    code cleanup removed empty lines and terminator where not required

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import {connect} from 'react-redux'
-
+import React        from 'react'
+import {connect}    from 'react-redux'
 const SongDetail = (props) => {
     const song = props.song
     if(!song)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,17 @@
 import {combineReducers} from "redux"
-
 export const SongsReducer = () => {
     return [
         {id:1, title: 'No Scrubs', duration: '4:05'},
         {id:2, title: 'Macarena', duration: '2:30'},
         {id:3, title: 'All Star', duration: '3:15'},
         {id:4, title: 'I Want it That Way', duration: '1:45'},
-    ];
+    ]
 }
-
 export const SelectedSongReducer = (selectedSong = null, action) => {
     if(action.type === 'SONG_SELECTED')
         return action.payload
     return selectedSong
 }
-
 export default combineReducers({
     songs: SongsReducer,
     selectedSong: SelectedSongReducer,
